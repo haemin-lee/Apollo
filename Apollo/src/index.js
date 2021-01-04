9497e6b3f069b1449f0711bc6aaf2a27a602d968
Devin Mui <devinwmui@gmail.com>
2021-01-03 22:54:27 -0800

    add keyboard avoiding view

--- Apollo/src/index.js
+++ Apollo/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
+    Keyboard,
+    KeyboardAvoidingView,
     StatusBar,
-    Text,
+    Pressable,
 } from 'react-native'
 
 import RootStackNavigator from '@app/navigation/RootStackNavigator.js'
 
 function App() {
     return (
-        <>
-            <StatusBar barStyle="dark-content" />
-            <RootStackNavigator />
-        </>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+            <Pressable onPress={() => Keyboard.dismiss()} style={{ flex: 1 }}>
+                <StatusBar barStyle="dark-content" />
+                <RootStackNavigator />
+            </Pressable>
+        </KeyboardAvoidingView>
     )
 }
 
--- Apollo/src/screens/auth/login.js
+++ Apollo/src/screens/auth/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TouchableOpacity, View } from 'react-native'
 
 import Button from '@app/components/buttons'
@@ -6,11 +6,26 @@ import { LinkText, Subtext } from '@app/components/text'
 import TextInput from '@app/components/text-input'
 
 function Login({ styles, onLinkPress }) {
+    const [contact, setContact] = useState('')
+    const [pass, setPass] = useState('')
+
+    const login = () => {}
+
     return (
         <View>
-            <TextInput placeholder="Email/Phone" style={styles.formItem} />
-            <TextInput placeholder="Password" style={styles.formItem} />
-            <Button style={styles.formItem}>
+            <TextInput
+                placeholder="Email/Phone"
+                style={styles.formItem}
+                value={contact}
+                onChangeText={(t) => setContact(t)}
+            />
+            <TextInput
+                placeholder="Password"
+                style={styles.formItem}
+                value={pass}
+                onChangeText={(t) => setPass(t)}
+            />
+            <Button style={styles.formItem} onPress={login}>
                 <Subtext>Login</Subtext>
             </Button>
 
