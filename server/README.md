2bc94dc752d74590b3cf096b8b139c7c31c9dd83
Devin Mui <devinwmui@gmail.com>
2021-01-05 04:54:28 -0800

    oauth token exchange should work

--- server/README.md
+++ server/README.md
@@ -8,7 +8,3 @@ This server hides API client credentials and does other back end stuff.
 https://drchrono.com/o/authorize/?redirect_uri=http://localhost:3000/oauth/login&response_type=code&client_id=dkzdPUi9XUYshx0batJXVE4zm8enBvFqlZCR0G2s
 ```
 
-### TODO
-
--   [] database
--   [] document API
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -4,4 +4,8 @@ export default {
     projectId: process.env.projectId, // google cloud project id
     db_connection_url: process.env.db_connection_url,
     drchrono_webhook_secret_token: process.env.drchrono_webhook_secret_token,
+    drchrono_authorize_path: process.env.drchrono_authorize_path,
+    drchrono_client_id: process.env.drchrono_client_id,
+    drchrono_client_secret: process.env.drchrono_client_secret,
+    drchrono_redirect_uri: process.env.drchrono_redirect_uri,
 }
