40c3334d6440fe87ab5c4c64c63a12559605020f
bcinbis <baran.cinbis1@gmail.com>
2021-01-11 22:32:50 -0700

    stinky

--- webapp/src/pages/home/tabs/images/index.js
+++ webapp/src/pages/home/tabs/images/index.js
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-
+/*
 function getType(props, filename) {
     if(filename.indexOf('.png') > -1)
     {
         return 
     }
 }
+*/
 
 function Images(props) {
     
     
-    let URI = "TESTINGTESTING";
+    //let URI = "TESTINGTESTING";
     
-    let source = "data:image/png;base64," + URI;
+    //let source = "data:image/png;base64," + URI;
     
     return (
         <h2>she lies</h2>
